1f81722fbb73d1c234fb0f604a034ffd6a3c97a6
aterlone <taneahaines@gmail.com>
2025-05-17 20:31:22 +1200

    Warp Zone

---
+++ mvm/warp_zone.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+@export var next_level_index = 0
+
+@export_enum (
+	"clean",
+	"waste",
+	"water",
+	"water-waste"
+) var room_name : String
+	
+@export var room_number : int
+
+@export_enum (
+	"up",
+	"down",
+	"left",
+	"right"
+   ) var direction : String
+	
+@export_enum (
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+) var room_char : String
