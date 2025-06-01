6230b5e5ceb9b3a42c56d2ec7626af7e7d97d9e8
metalgemmys <crystalmetallics@gmail.com>
2025-06-01 19:16:33 +1200

    completed powerup collectable

--- mvm/LevelGeneral/powerup_collectable.gd
+++ mvm/LevelGeneral/powerup_collectable.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var PLAYER = get_tree().get_root().get_node("Main/Player")
+
 @export_enum (
 	"harden",
 	"bubble",
@@ -7,7 +9,39 @@ extends Area2D
 ) var powerup_type : String
 
 
+var colors = {
+	"harden" : Color.RED,
+	"bubble" : Color.BLUE,
+	"swim" : Color.GREEN
+	
+}
+
+
+func _physics_process(delta: float) -> void:
+	$Gem.modulate = colors[powerup_type]
+	$PowerupAnimation/Gem.modulate = colors[powerup_type]
+
+
+func play_anim():
+	$Gem.visible = false
+	get_tree().paused = true
+	
+	PLAYER.global_position = global_position
+	PLAYER.visible = false
+	
+	$PowerupAnimation/AnimationPlayer.play("PowerupHarden")
+	await $PowerupAnimation/AnimationPlayer.animation_finished
+	
+	PLAYER.global_position = $PowerupAnimation.global_position
+	PLAYER.visible = true
+	
+	get_tree().paused = false
+	queue_free()
+
 func _on_area_entered(area: Area2D) -> void:
+	
+	
 	if area.get_parent().name == "Player":
 		area.get_parent().update_powerups(powerup_type)
-		queue_free()
+	
+	play_anim()
